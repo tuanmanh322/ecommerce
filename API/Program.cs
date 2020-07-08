56e6996d9eb4e585635ab4335afe49cdacf8a073
Jim Shaw <jimxshaw@gmail.com>
2020-07-07 19:41:35 -0500

    Refactor Program's Main method so migrations are automatically run upon application start up.

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,47 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace API
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+      var host = CreateHostBuilder(args).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+      using (var scope = host.Services.CreateScope())
+      {
+        var services = scope.ServiceProvider;
+        var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+        try
+        {
+          var context = services.GetRequiredService<StoreContext>();
+
+          // Run any migrations upon application start up.
+          await context.Database.MigrateAsync();
+        }
+        catch (Exception ex)
+        {
+          var logger = loggerFactory.CreateLogger<Program>();
+          logger.LogError(ex, "An error occurred during migration.");
+        }
+      }
+
+      host.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            });
+  }
 }
