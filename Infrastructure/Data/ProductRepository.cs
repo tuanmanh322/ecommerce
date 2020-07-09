572282ca781c7672f595a0c01cabc2d545358919
Jim Shaw <jimxshaw@gmail.com>
2020-07-09 18:45:21 -0500

    Add SpecificationEvaluator class in order to implement the Specification Pattern.

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -17,7 +17,7 @@ namespace Infrastructure.Data
 
     public async Task<Product> GetProductByIdAsync(int id)
     {
-      // The Specification Pattern (ISpecification, BaseSpecification) is
+      // The Specification Pattern (ISpecification, BaseSpecification, SpecificationEvaluator) is
       // all done to give the ability to use Include statements.
       return await _context.Products
                             .Include(p => p.ProductBrand)
