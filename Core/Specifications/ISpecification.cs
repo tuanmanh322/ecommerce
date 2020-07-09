147aef22f1b5f4fb7e61aff1afaf126b560fdcea
Jim Shaw <jimxshaw@gmail.com>
2020-07-09 18:34:57 -0500

    Add BaseSpecification class in order to implement the Specification Pattern to bolster the Generic Repository pattern.

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -6,6 +6,8 @@ namespace Core.Specifications
 {
   public interface ISpecification<T>
   {
+    // The Specification Pattern will fix certain shortfalls
+    // of the Generic Repository pattern.
     Expression<Func<T, bool>> Criteria { get; }
 
     List<Expression<Func<T, object>>> Includes { get; }
