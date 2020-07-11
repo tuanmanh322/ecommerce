bcc893ebf1e36369e2c7edb89922b85e9375a524
Jim Shaw <jimxshaw@gmail.com>
2020-07-11 13:35:44 -0500

    In Startup, register a service configuration to override the default [ApiController] attribute response with our custom response.

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -41,6 +42,28 @@ namespace API
 
       services.AddDbContext<StoreContext>(options =>
                                             options.UseNpgsql(_config.GetConnectionString("DefaultConnection")));
+
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        // Be sure to register this service configuration AFTER
+        // services.AddControllers().
+        // This entire services configuration is done to override
+        // the [ApiController] attribute, which is classified as
+        // a ModelState response, with our own response.
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+          string[] errors = actionContext.ModelState.Where(e => e.Value.Errors.Count > 0)
+                                                    .SelectMany(x => x.Value.Errors)
+                                                    .Select(x => x.ErrorMessage).ToArray();
+
+          var errorResponse = new ApiValidationErrorResponse()
+          {
+            Errors = errors
+          };
+
+          return new BadRequestObjectResult(errorResponse);
+        };
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
