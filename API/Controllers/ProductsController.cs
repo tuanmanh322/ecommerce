bb81e4ab63b5b306ec285c2715fcf4c8937070ec
Jim Shaw <jimxshaw@gmail.com>
2020-07-12 12:36:42 -0500

    Add ProductSpecParams class to use instead of query strings.

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -31,7 +31,9 @@ namespace API.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
+    public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort,
+                                                                                    int? brandId,
+                                                                                    int? typeId)
     {
       var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
 
---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,26 @@
+namespace Core.Specifications
+{
+  public class ProductSpecParams
+  {
+    private const int MaxPageSize = 50;
+
+    // Set the default page size. Users are
+    // free to choose any size 1 <= n <= 50.
+    private int _pageSize = 6;
+
+    public int PageSize
+    {
+      get => _pageSize;
+      set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+
+    // Always return the first page.
+    public int PageIndex { get; set; } = 1;
+
+    public int? BrandId { get; set; }
+
+    public int? TypeId { get; set; }
+
+    public string Sort { get; set; }
+  }
+}
