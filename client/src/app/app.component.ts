0a8a3b54ec464dc04d2db274d3b98ea9d1997d05
Jim Shaw <jimxshaw@gmail.com>
2020-07-17 16:57:08 -0500

    Put models folder in app folder.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IProduct } from '../models/product';
+import { IProduct } from './models/product';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: any) => {
+      (response: IPagination) => {
         this.products = response.data;
       },
       (error) => {
