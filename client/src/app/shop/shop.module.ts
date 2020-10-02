2efc33269352f8418930dcdfb108c8146c4be1f8
Jim Shaw <jimxshaw@gmail.com>
2020-10-02 11:38:25 -0500

    Add shop component.

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShopComponent } from './shop.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ShopComponent],
   imports: [
     CommonModule
   ]
