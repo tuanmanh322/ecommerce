3fc44862df7b221251a78eeb47303cfc02727780
Jim Shaw <jimxshaw@gmail.com>
2020-10-07 09:04:24 -0500

    Add query string to display all products.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -12,6 +12,6 @@ export class ShopService {
   constructor(private http: HttpClient) { }
 
   getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+    return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
   }
 }
