ff190672b9cd312e9dcf02a000157213fd020d36
Jim Shaw <jimxshaw@gmail.com>
2020-11-21 17:26:28 -0600

    Add IBrand and IType interfaces, add getBrands & getTypes methods in Shop Service.

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
+import { IBrand } from '../shared/models/brand';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands() {
+    return this.http.get<IBrand>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<IType>(this.baseUrl + 'products/types');
   }
 }
